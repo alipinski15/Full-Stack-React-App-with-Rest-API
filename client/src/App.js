011937582ed3bcbcee2b1ea574ce98f2253a2fd6
alipinski15 <58602252+alipinski15@users.noreply.github.com>
2020-08-10 21:16:21 -0700

    Update
    Fixed not found route and added CSS styling.

--- client/src/App.js
+++ client/src/App.js
@@ -49,7 +49,7 @@ export default () => (
         <Route path="/signout" component={UserSignOutWithContext} />
         <Route path="/error" component={UnhandledError} />
         <Route path='/forbidden' component={Forbidden} />
-        <Route path='/notfound' component={NotFound} />
+        <Route component={NotFound} />
       </Switch>
     </div>
   </Router>
--- client/src/components/UpdateCourse.js
+++ client/src/components/UpdateCourse.js
@@ -21,7 +21,6 @@ export default class UpdateCourse extends Component {
   }
   
 
-  
   componentDidMount() {
     const { context } = this.props;
     const authUser = this.props.context.authenticatedUser;
@@ -43,7 +42,6 @@ export default class UpdateCourse extends Component {
     })
     .catch((err) => {
       console.log(err);
-      this.props.history.push('/notfound')
     });
   }
 
